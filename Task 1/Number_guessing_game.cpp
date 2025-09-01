4ab508a88588fa8b4218b745fa0f2ce97f6b2331
Sanskarbawniya <sanskarbawniya@gmail.com>
2025-09-01 14:45:06 +0530

    Update Number_guessing_game.cpp

--- Task 1/Number_guessing_game.cpp
+++ Task 1/Number_guessing_game.cpp
@@ -25,3 +25,61 @@ int main() {
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <iostream>
+using namespace std;
+int main(){
+    int a[5]={3,2,1,5,8};
+    int val,i,pos, flag=0;
+    cout<<"Array elements :";
+    for(i=0;i<5;i++)
+    {
+        cout<<a[i]<<endl;
+    }
+    cout<<"enter a number to search";
+    cin>>val;
+    i=0;
+    while(i<5)
+    {
+        if(a[i]==val)
+        {
+            cout<<"elements founded at index :"<<i+1;
+            i++;
+            flag=1;
+        }
+        else{
+            i++;
+        }
+    }
+    if(flag==0)
+    cout<<"search is unsuccessful";
+    
+    if(pos<1,pos>5){
+        cout<<"invalid position"<<endl;
+    }else{
+        for(i=pos-1;i<5-1;i++){
+            cout<<a[5]<<"";
+        }
+        cout<<endl;
+    }
+    return 0;
+
+}
